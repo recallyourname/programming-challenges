35a504a8775a6b76ec49d8c446d9ae73176d0019
recallyourname <vedkalnikita@gmail.com>
2019-03-24 14:24:23 +0600

    modify 6

--- [6]rps.py
+++ [6]rps.py
@@ -8,7 +8,9 @@ computer = secrets.choice(variants)
 
 if player == computer:
     print("computer shows: " + computer + "tie")
-elif (player == 'rock' and computer != 'paper') or (player == 'paper' and computer != 'scissors') or (player == 'scissors' and computer != 'rock'):
+elif (player == 'rock' and computer != 'paper')\
+    or (player == 'paper' and computer != 'scissors')\
+    or (player == 'scissors' and computer != 'rock'):
     print("computer shows: " + computer + ", You win!")
 else:
     print("Computer shows: " + computer + ", You lost!")
