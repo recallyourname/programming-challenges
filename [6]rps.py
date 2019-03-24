672c94824f43c311ce37c198584842f33f2a7727
recallyourname <vedkalnikita@gmail.com>
2019-03-24 14:21:54 +0600

    modify 6

--- [6]rps.py
+++ [6]rps.py
@@ -6,7 +6,6 @@ variants = ['rock', 'paper', 'scissors']
 
 computer = secrets.choice(variants)
 
-
 if player == computer:
     print("computer shows: " + computer + "tie")
 elif (player == 'rock' and computer != 'paper') or (player == 'paper' and computer != 'scissors') or (player == 'scissors' and computer != 'rock'):
