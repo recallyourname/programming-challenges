55774ad4b9e95b8655976aa5e913a7e0e980692e
recallyourname <vedkalnikita@gmail.com>
2019-03-26 19:48:59 +0600

    labels and separate function for directory to clearify code

--- [11]passwordgen.py
+++ [11]passwordgen.py
@@ -1,6 +1,14 @@
 import os
 import secrets
 
+def filedirectory():
+    try:
+        os.mkdir("passwords")
+        print("Directory passwords created")
+    except FileExistsError:
+        print("Directory passwords already exist")    
+
+
 def password(num):
     data = ""
     availableSymbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_=+[].,?"
@@ -10,17 +18,13 @@ def password(num):
 
 
 def main():
-    try:
-        os.mkdir("passwords")
-        print("Directory passwords created")
-    except FileExistsError:
-        print("Directory passwords already exist")
-
+    filedirectory()
     directory = os.getcwd()
-    n = int(input("Input the number of symbols in password: "))
-    with open(directory+'/passwords/password.txt','w+') as f:
-        data = password(n)
-        f.write(data+"\n")
+    numbers = int(input("Input the number of symbols in password: "))
+    label = input("Put a label for your password here(where it will be used): ")
+    with open(directory+'/passwords/password.txt','a+') as f:
+        data = password(numbers)
+        f.write("Password for "+label+" is: "+data+"\n")
         f.close()
     print(data)
     print("Password also stored in /filedirectory/passwords")
